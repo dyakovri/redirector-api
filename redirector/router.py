1e206ef537aea4d8da1fb33ecb973c7eb343e44b
Roman Dyakov <ROMAN@DYAKOV.SPACE>
2023-06-17 20:30:38 +0200

    Log task

--- redirector/router.py
+++ redirector/router.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, BackgroundTasks, HTTPException
+from fastapi import APIRouter, BackgroundTasks, HTTPException, Request
 from fastapi_sqlalchemy import db
 from starlette.responses import RedirectResponse, Response
+import user_agents
 
-from .models import Link
+from .models import Link, RedirectFact
 from .schemas import NewRedirectUrl
 from .settings import get_settings
 
@@ -36,8 +37,21 @@ def create_route(secret: str, url_from: str):
     return Response(status_code=204)
 
 
-def log_redirect():
-    pass
+def log_redirect(link_id, method, user_agent):
+    ua = user_agents.parse(user_agent)
+    fact = RedirectFact()
+    fact.link_id = link_id
+    fact.method = method
+    fact.user_agent = user_agent
+    fact.browser_family = ua.browser.family
+    fact.browser_version = ua.browser.version_string
+    fact.os_family = ua.os.family
+    fact.os_version = ua.os.version_string
+    fact.device_family = ua.device.family
+    fact.device_brand = ua.device.brand
+    fact.device_model = ua.device.model
+    db.session.add(fact)
+    db.session.commit()
 
 
 @router.api_route(
@@ -45,7 +59,7 @@ def log_redirect():
     methods=["GET", "POST", "PUT", "PATCH", "DELETE"],
     status_code=307,
 )
-def redirect(url_from: str, background_tasks: BackgroundTasks):
+def redirect(request: Request, url_from: str, background_tasks: BackgroundTasks):
     if url_from == '':
         return RedirectResponse('/ui/')
     redir_obj = (
@@ -54,5 +68,5 @@ def redirect(url_from: str, background_tasks: BackgroundTasks):
     if not redir_obj:
         raise HTTPException(404, "Not found")
 
-    background_tasks.add_task(log_redirect)
+    background_tasks.add_task(log_redirect, redir_obj.id, request.method, request.headers["User-Agent"])
     return RedirectResponse(redir_obj.url_to)
